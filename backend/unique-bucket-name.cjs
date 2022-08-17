2443311e1e4d57e9369333aad9175e2cfe07be2d
Luciano Mammino <lucianomammino@gmail.com>
2022-08-17 17:48:16 +0100

    added serverless yml and bucket iac

---
+++ backend/unique-bucket-name.cjs
@@ -0,0 +1,12 @@
+const { createHash } = require('crypto')
+
+module.exports = async function ({ options, resolveVariable }) {
+  const account = await resolveVariable('aws:accountId')
+  const region = await resolveVariable('aws:region')
+  const stage = await resolveVariable('sls:stage')
+  const input = `weshare-${account}-${region}-${stage}`
+  const bucketName = 'weshare-' + createHash('md5').update(input).digest('hex')
+  return {
+    bucketName
+  }
+}

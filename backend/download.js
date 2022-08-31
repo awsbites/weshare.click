4fcc3b56beddef891ebfa1eae5705f515bc26f23
Luciano Mammino <lucianomammino@gmail.com>
2022-08-31 18:07:41 +0100

    Added middy, power tools and content negotiation

--- backend/download.js
+++ backend/download.js
@@ -1,19 +1,21 @@
+import { Metrics, logMetrics, MetricUnits } from '@aws-lambda-powertools/metrics'
+import { Tracer, captureLambdaHandler } from '@aws-lambda-powertools/tracer'
+import { Logger, injectLambdaContext } from '@aws-lambda-powertools/logger'
+import middy from '@middy/core'
 import { S3Client, GetObjectCommand } from '@aws-sdk/client-s3'
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner'
 
 const { BUCKET_NAME } = process.env
 const EXPIRY_DEFAULT = 60
+
+const tracer = new Tracer()
+const logger = new Logger()
+const metrics = new Metrics()
 const s3Client = new S3Client()
 
-export const handleEvent = async (event, context) => {
+async function handler (event, context) {
   // extract the id from the URL (path parameter called id)
   const id = event.pathParameters.id
-  if (!id) {
-    return {
-      statusCode: 400,
-      body: 'Missing id'
-    }
-  }
 
   // create a presigned URL for the file in the bucket with the given id
   const key = `shares/${id[0]}/${id[1]}/${id}`
@@ -26,6 +28,9 @@ export const handleEvent = async (event, context) => {
     expiresIn: EXPIRY_DEFAULT
   })
 
+  logger.info('Downloading share', { id, key })
+  metrics.addMetric('downloadShare', MetricUnits.Count, 1)
+
   // return an HTTP redirect response to the presigned URL
   return {
     statusCode: 301,
@@ -34,3 +39,8 @@ export const handleEvent = async (event, context) => {
     }
   }
 }
+
+export const handleEvent = middy(handler)
+  .use(injectLambdaContext(logger, { logEvents: true }))
+  .use(logMetrics(metrics))
+  .use(captureLambdaHandler(tracer))
--- backend/share-handler.js
+++ backend/share-handler.js
@@ -1,3 +1,9 @@
+import { Metrics, logMetrics, MetricUnits } from '@aws-lambda-powertools/metrics'
+import { Tracer, captureLambdaHandler } from '@aws-lambda-powertools/tracer'
+import { Logger, injectLambdaContext } from '@aws-lambda-powertools/logger'
+import middy from '@middy/core'
+import httpContentNegotiation from '@middy/http-content-negotiation'
+import httpHeaderNormalizer from '@middy/http-header-normalizer'
 import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3'
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner'
 import { randomUUID } from 'node:crypto'
@@ -6,9 +12,12 @@ const { BUCKET_NAME, BASE_URL } = process.env
 const EXPIRY_DEFAULT = 24 * 60 * 60
 const MIME_TYPE = 'application/octet-stream'
 
+const tracer = new Tracer()
+const logger = new Logger()
+const metrics = new Metrics()
 const s3Client = new S3Client()
 
-export const handleEvent = async (event, context) => {
+async function handler (event, context) {
   // Create a key (file name)
   const id = randomUUID()
   const key = `shares/${id[0]}/${id[1]}/${id}`
@@ -16,6 +25,9 @@ export const handleEvent = async (event, context) => {
   const filename = event?.queryStringParameters?.filename
   const contentDisposition = `attachment; filename="${filename}"`
   const contentDispositionHeader = contentDisposition && `content-disposition: ${contentDisposition}`
+
+  logger.info('Creating share', { id, key, filename, contentDispositionHeader })
+  metrics.addMetric('createShare', MetricUnits.Count, 1)
 
   // Create the download URL
   const downloadUrl = `${BASE_URL}/share/${id}`
@@ -39,12 +51,44 @@ export const handleEvent = async (event, context) => {
     }
   )
 
+  let headers = {
+    'content-type': 'text/plain'
+  }
+
+  let body = `
+  Upload with: curl -X PUT -T ${filename || '<FILENAME>'} ${contentDispositionHeader ? `-H '${contentDispositionHeader}'` : ''} '${uploadUrl}' 
+  
+  Download with: curl ${downloadUrl}
+  `
+
+  if (event.preferredMediaType === 'application/json') {
+    body = JSON.stringify({
+      filename,
+      headers: [contentDispositionHeader],
+      uploadUrl,
+      downloadUrl
+    })
+    headers = {
+      'content-type': 'application/json'
+    }
+  }
+
   return {
     statusCode: 201,
-    body: `
-Upload with: curl -X PUT -T ${filename || '<FILENAME>'} ${contentDispositionHeader ? `-H '${contentDispositionHeader}'` : ''} '${uploadUrl}' 
-
-Download with: curl ${downloadUrl}
-`
+    headers,
+    body
   }
 }
+
+export const handleEvent = middy(handler)
+  .use(injectLambdaContext(logger, { logEvents: true }))
+  .use(logMetrics(metrics))
+  .use(captureLambdaHandler(tracer))
+  .use(httpHeaderNormalizer())
+  .use(httpContentNegotiation({
+    parseCharsets: false,
+    parseEncodings: false,
+    parseLanguages: false,
+    failOnMismatch: false,
+    availableMediaTypes: ['text/plain', 'application/json']
+  }))

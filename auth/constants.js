23fbce1cef7af246c5a1822d7c01336a7ea7cfbd
Eoin Shanaghy <eoin.shanaghy@fourtheorem.com>
2022-09-28 10:49:17 +0100

    Add IdP callback and /token endpoints

--- auth/constants.js
+++ auth/constants.js
@@ -4,3 +4,18 @@ export const DeviceAuthStatus = {
   CODE_ISSUED: 'code_issued',
   ERROR: 'error'
 }
+
+export const OAuthErrorCodes = {
+  // Device Access Token Response error codes (https://www.rfc-editor.org/rfc/rfc8628#section-3.5)
+  AUTHORIZATION_PENDING: 'authorization_pending',
+  EXPIRED_TOKEN: 'expired_token',
+  ACCESS_DENIED: 'access_denied',
+  SLOW_DOWN: 'slow_down',
+  // OAuth 2.0 Response error codes (https://www.rfc-editor.org/rfc/rfc6749#section-5.2)
+  INVALID_REQUEST: 'invalid_request',
+  INVALID_CLIENT: 'invalid_client',
+  INVALID_GRANT: 'invalid_grant',
+  UNAUTHORIZED_CLIENT: 'unauthorized_client',
+  UNSUPPORTED_GRANT_TYPE: 'unsupported_grant_type',
+  INVALID_SCOPE: 'invalid_scope'
+}

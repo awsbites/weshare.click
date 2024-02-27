c23b966a905604eb63d9ab5c94765bbd633fbbad
Eoin Shanaghy <eoin.shanaghy@fourtheorem.com>
2024-02-27 09:00:10 +0000

    chore: style hosted UI similar to frontend

--- auth/user-pool-css.cjs
+++ auth/user-pool-css.cjs
@@ -1,7 +1,37 @@
 module.exports = () => `
+.logo-customizable {
+  max-width: 20%;
+  max-height: 20%;
+}
+
 .banner-customizable {
-  padding: 10px 0px 10px 0px;
-  background-color: #5E17EB;
+  padding: 25px 0px 25px 0px;
+  background-color: rgb(250, 250, 250);
+  border-radius: 6px;
+}
+
+.label-customizable {
+  font-weight: 400;
+}
+
+.textDescription-customizable {
+  color: rgb(113, 113, 122);
+  padding-top: 10px;
+  padding-bottom: 16px;
+  display: block;
+  font-size: 14px;
+}
+
+.idpDescription-customizable {
+  padding-top: 10px;
+  padding-bottom: 10px;
+  display: block;
+  font-size: 16px;
+}
+
+.legalText-customizable {
+  color: rgb(113, 113, 122);
+  font-size: 11px;
 }
 
 .submitButton-customizable {
@@ -11,11 +41,81 @@ module.exports = () => `
   height: 40px;
   width: 100%;
   color: #fff;
-  background-color: #5E17EB;
+  background-color: rgb(250, 250, 250);
+}
+
+.errorMessage-customizable {
+  padding: 5px 0;
+  font-size: 14px;
+  width: 100%;
+  background: rgb(250, 250, 250);
+  color: rgb(200, 0, 0);
+  border: none;
+}
+
+.inputField-customizable {
+  width: 100%;
+  height: 34px;
+  color: rgb(9, 9, 11);
+  font-size: 16px;
+  border-radius: 2px;
+  background-color: rgb(250, 250, 250);
+  border: 1px solid;
+  border-color: rgb(210, 210, 215);
+  border-radius: 6px;
+  box-shadow: none;
+  padding: 6px;
+  height: 40px;
+}
+
+.inputField-customizable:focus {
+  border-color: rgb(9, 9, 11);
+  border-width: 2px;
+  outline: 0;
+  box-shadow: none;
+}
+
+.submitButton-customizable {
+  height: 40px;
+  width: 100%;
+  text-align: center;
+  margin-bottom: 15px;
+  background-color: rgb(9, 9, 11);
+  color: rgb(250, 250, 250);
+  border-color: rgb(250, 250, 250);
+  font-weight: 400;
 }
 
 .submitButton-customizable:hover {
-  color: #fff;
-  font-weight: normal;
-  background-color: #5E17EB;
-}`
+  background-color: rgb(24, 24, 27);
+}
+
+.socialButton-customizable {
+  height: 40px;
+  text-align: left;
+  width: 100%;
+  margin-bottom: 15px;
+}
+
+.redirect-customizable {
+  display: none;
+  text-align: center;
+}
+
+.passwordCheck-notValid-customizable {
+  color: #DF3312;
+}
+
+.passwordCheck-valid-customizable {
+  color: #19BF00;
+}
+
+.background-customizable {
+  background-color: rgb(250, 250, 250);
+  border-radius: 6px;
+}
+
+.label-customizable {
+  font-weight: 800;
+  padding-top: 16px;
+} `
